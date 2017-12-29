40aecc4adff77fb90c98144ccb19c2e34edde425
mterron <miguel.a.terron@gmail.com>
2017-12-29 17:48:40 +1300

    * Fix extended attributes due to Docker lack of support for persisting them on build.

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+# Fix nodejs PaX extended attributes
+attr -s pax.flags -V em $(which node)
+
 if [ ! -f /usr/app/horizon-key.pem ] && [ ${HZ_SECURE:-yes} == 'yes' ]; then
 	echo "Generating Horizon app certificate"
 	hz create-cert
 fi
+
 chown -R daemon:daemon /usr/app
 exec su-exec daemon:daemon hz serve --bind all -p "${HZ_PORT:-8181}" --connect "${RETHINKDB_URI:-rethinkdb://rethinkdb:28015}" /usr/app
